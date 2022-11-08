f54a4585a7bf1234f4c0c90170a93feb62d97344
sourov <sourov.jsr98@gmail.com>
2022-11-08 16:19:15 +0600

    user created in mondodb and dotenv added

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+require('dotenv').config();
 // middleware
 app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.ehoamog.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ehoamog.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
     const collection = client.db("test").collection("devices");
