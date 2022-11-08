90ab612d014a195e1d22f7b041cfd1a03aec997b
sourov <sourov.jsr98@gmail.com>
2022-11-08 21:31:23 +0600

    sending one service data to client side

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 // middleware
 app.use(cors());
@@ -22,7 +22,16 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
 
-        })
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+
     }
     finally {
 
