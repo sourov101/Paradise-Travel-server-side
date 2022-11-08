dcb15a18aa433df315ac6e95be39674f0d54d568
sourov <sourov.jsr98@gmail.com>
2022-11-08 17:27:58 +0600

    paradisedb database created in mongodb

--- index.js
+++ index.js
@@ -11,11 +11,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ehoamog.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
 
 
 app.get('/', (req, res) => {
