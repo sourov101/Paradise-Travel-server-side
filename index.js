fc57d4eb19d0bbf63d6476c8d8e405556eab11d9
sourov <sourov.jsr98@gmail.com>
2022-11-10 18:16:44 +0600

    update modified

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -12,9 +14,36 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ehoamog.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader;
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
 async function run() {
     try {
         const serviceCollection = client.db('paradiseDb').collection('services');
+        const reviewCollection = client.db('paradiseDb').collection('review');
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            console.log(user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+            return res.send({ token })
+
+
+        })
 
         app.get('/services', async (req, res) => {
             const query = {}
@@ -25,7 +54,8 @@ async function run() {
         });
         app.get('/homeServices', async (req, res) => {
             const query = {}
-            const cursor = serviceCollection.find(query);
+            const sort = { _id: -1 };
+            const cursor = serviceCollection.find(query).sort(sort);
             const services = await cursor.limit(3).toArray();
             res.send(services);
 
@@ -45,22 +75,71 @@ async function run() {
         });
 
 
-        const reviewCollection = client.db('paradiseDb').collection('review');
+
 
         app.post('/reviews', async (req, res) => {
+
             const addedReview = req.body;
             const result = await reviewCollection.insertOne(addedReview);
-            res.send(result);
+            return res.send(result);
         });
 
         app.get('/reviews', async (req, res) => {
+
+
+
             const query = {}
             const sort = { date: -1 };
             const cursor = reviewCollection.find(query).sort(sort);
             const reviews = await cursor.toArray();
-            res.send(reviews);
+            return res.send(reviews);
 
         });
+
+        app.get('/reviews/:id', async (req, res) => {
+
+            const decoded = req.decoded;
+            if (decoded?.email !== req.query.email) {
+
+                return res.status(401).send({ message: 'unauthorized access' })
+
+            }
+
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await reviewCollection.findOne(query);
+            return res.send(result);
+
+        })
+
+        app.delete('/reviews/:id', async (req, res) => {
+
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await reviewCollection.deleteOne(query);
+            return res.send(result);
+
+        })
+
+        app.put('/updateReview/:id', async (req, res) => {
+
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const reviews = req.body;
+
+            const option = { upsert: true };
+            const updatedReview = {
+                $set: {
+
+                    rating: reviews.rating,
+                    review: reviews.review,
+                }
+            }
+
+            const result = await reviewCollection.updateOne(query, updatedReview, option);
+            return res.send(result);
+
+        })
 
     }
     finally {
